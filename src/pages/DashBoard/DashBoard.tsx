60253427fb8eeec3cfee6c371d7272426a0840b5
Julián Camilo Alfonso Carrillo <julcalfonsocar@unal.edu.co>
2023-04-24 16:03:50 -0500

    Intento consumir del back

--- src/pages/DashBoard/DashBoard.tsx
+++ src/pages/DashBoard/DashBoard.tsx
@@ -1,9 +1,16 @@
 import { CardComponent } from '@/components'
 import React from 'react'
+import get from "@/services/axiosService";
 
 const DashBoard : React.FC<{}> = () => {
 
-    //cost [] = React.useState<>(null)
+  React.useEffect(()=>{
+    get().then((r)=>{
+      console.log(r.data.results)
+    }).catch((e)=>{
+      console.error(e)
+    })
+  },[])
 
   return (
     <div>
